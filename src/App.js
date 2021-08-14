f0f2693f44197cd6f2c6e3550b605c50cc24d7d8
Luiz Verissimo <luizverissimosouza@gmail.com>
2021-08-14 16:39:05 -0300

    finish CRUD repositories

--- src/App.js
+++ src/App.js
@@ -1,26 +1,58 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import api from "./services/api";
 
 import "./styles.css";
 
 function App() {
+  const [repositories, setRepositories] = useState([]);
+
+  function getData() {
+    api
+      .get("repositories")
+      .then((response) => setRepositories(response.data))
+      .catch((error) => console.log(error));
+  }
+
+  useEffect(() => {
+    getData();
+  }, []);
+
   async function handleAddRepository() {
-    // TODO
+    await api
+      .post(`repositories`, {
+        title: `semana-omni-stack3-${Date.now()}`,
+        url: "git@github.com:luizverissimo/semanaomnistack.git",
+        techs: ["nodejs", "postgresql"],
+      })
+      .then((response) => {
+        setRepositories([...repositories, response.data]);
+      })
+      .catch((error) => console.log(error));
   }
 
   async function handleRemoveRepository(id) {
-    // TODO
+    await api
+      .delete(`repositories/${id}`)
+      .then(() => {
+        const repositoriesFiltered = repositories.filter(
+          (repository) => id != repository.id
+        );
+        setRepositories(repositoriesFiltered);
+      })
+      .catch((error) => console.log(error));
   }
 
   return (
     <div>
       <ul data-testid="repository-list">
-        <li>
-          Repositório 1
-
-          <button onClick={() => handleRemoveRepository(1)}>
-            Remover
-          </button>
-        </li>
+        {repositories.map((repository) => (
+          <div key={repository.id}>
+            <li>{repository.title}</li>
+            <button onClick={() => handleRemoveRepository(repository.id)}>
+              Remover
+            </button>
+          </div>
+        ))}
       </ul>
 
       <button onClick={handleAddRepository}>Adicionar</button>
